00ae43f58bf2d90b28d1501403f0772ca78536bb
hkidh <hki.dh4@gmail.com>
2021-06-23 16:46:08 +0800

    tp.py

---
+++ tp.py
@@ -0,0 +1,42 @@
+
+# read_file
+data = []                       # 開一個空清單
+with open('tp.txt', 'r') as f:  # 開啟檔案,讀進檔案 tp.txt
+    for line in f:              # 在檔案中每一行一行
+        data.append(line.strip()) # 把每行去除換行符號,一行行加入data清單中
+print('Policy 共有' + str(len(data)) + '行資料') # 計算data有多少行的資料,將 int 改為 str
+
+# write_file
+with open('tp.csv', 'w') as f: # 開啟檔案,寫入檔案tp.csv 
+    for line in data:
+        if line == '!':         #有!的行,跳過
+            continue
+        if len(line) != 0:      # 在行有字元
+            if 'protect zone' in line: #有'protect zone'字的行
+                    f.write(line + '\n') #寫進f檔
+
+# sort_and_policy_name_list
+csv = []
+one = []
+with open('tp.csv', 'r') as f:
+    for line in f:
+        csv.append(line.split(' '))
+    for line in csv:
+        one.append(line[2])
+    x = sorted(set(one)) #去除重複值+排序
+    for line in x:
+        print(line)
+
+
+# filter_Policy_and_list
+with open('tp.csv', 'r') as f:
+    print('---------------------')
+    gn = input('請輸入 Group_name: ')
+    for line in f:
+        if gn in line:
+            if '-group_ip' in line:
+                continue
+            print(line.strip())
+        
+
+
